4d563f1d7cc462f6373d46852e931c37fd8bdce8
Aleksei Mihhailov <aleksei.mihhailov@gmail.com>
2011-12-21 04:00:53 +0200

    Modified 'BillingSystem' project fiels.

--- BilllingSystem/BilllingMachine/Models/Rates.cs
+++ BilllingSystem/BilllingMachine/Models/Rates.cs
@@ -10,16 +10,7 @@ namespace BilllingMachine.Models
         // Default constructor
         public Rates() { }
 
-        //public Rates(string direction, string mPrice, string fPrice) 
-        //{
-        //    this.Direction = direction;
-        //    this.MobilePrice = mPrice;
-        //    this.FixedPrice = fPrice;
-        //}
-
         // Properties
         public string Direction { get; set; }
-        //public string MobilePrice { get; set; }
-        //public string FixedPrice { get; set; }
     }
 }
